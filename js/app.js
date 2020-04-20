3a522c90a7abb23b48957f70ff154de5e4959009
Andrea <andrea270696@gmail.com>
2020-04-20 00:49:17 -0500

    archivos

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function validate() {
+    var spinner=document.getElementById('spinner');
+    setTimeout(function(){
+        spinner.style.display="none";
+    }, 3000);
+    return false;
+  }
+
+  validate();
